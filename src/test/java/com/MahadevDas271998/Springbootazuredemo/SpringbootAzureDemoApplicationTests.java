9d69dc089aa44773350d185fa7c47836bfe71814
@priyanshudhar4 <priyanshudhar4@gmail.com>
2021-12-31 16:08:19 +0530

    Springboot-azure-demo

---
+++ src/test/java/com/MahadevDas271998/Springbootazuredemo/SpringbootAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MahadevDas271998.Springbootazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
